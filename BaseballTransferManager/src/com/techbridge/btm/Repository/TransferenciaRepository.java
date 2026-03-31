2973b19d95a32b75b1cd8148388329b442dec983
Gilber Vido <gilbert14ot@gmail.com>
2026-03-31 14:17:26 -0400

    Create TransferenciaRepository interface

---
+++ BaseballTransferManager/src/com/techbridge/btm/Repository/TransferenciaRepository.java
@@ -0,0 +1,17 @@
+package com.techbridge.btm.repository;
+
+/**
+ *
+ * @author gilber
+ */
+public interface TransferenciaRepository {
+    // Aqui creamos este metodo para poder guardar una transferencia dado por el GUI
+    void guardarTransferencia(Transferencia transfe); 
+    
+    // Esto lo creamos para poder buscar una transferencia por su id correspondiente
+    Transferencia buscarTransferencia(int idTransferencia);
+    
+    // Este otro metodo lo creamos para eliminar una transferencia
+    void eliminarTransferencia(int idTransferencia);
+    
+}
